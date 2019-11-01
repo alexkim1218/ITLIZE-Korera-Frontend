96b1c318dedc2922e97b6942668aca8f2feab84c
christhoperbernard <chbernar@ucsd.edu>
2019-11-01 14:38:34 -0400

    initial file, with login and signup page

---
+++ korera/src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent{
+
+  constructor() { }
+
+  onSubmit(f: NgForm) {
+    console.log(f.value);  // { first: '', last: '' }
+    console.log(f.valid);  // false
+  }
+
+  log(x: string) {
+    console.log(x);
+  }
+}
---
+++ korera/src/app/signup/signup.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+
+  constructor() { }
+
+  onSubmit(f: NgForm) {
+    console.log(f.value);  // { first: '', last: '' }
+    console.log(f.valid);  // false
+  }
+
+  log(x: string) {
+    console.log(x);
+  }
+
+}
